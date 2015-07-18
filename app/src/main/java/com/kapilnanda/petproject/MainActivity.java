7747c3bec15ff565e6724d6a0fde9f9b69399999
Kapil Nanda <mail.kapilnanda@gmail.com>
2015-07-19 04:10:48 +0530

    main lAYOUT modified

--- app/src/main/java/com/kapilnanda/petproject/MainActivity.java
+++ app/src/main/java/com/kapilnanda/petproject/MainActivity.java
@@ -1,20 +1,22 @@
 package com.kapilnanda.petproject;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private String buttonText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button
 
     }
 
@@ -39,5 +41,34 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void sendMessage(View view) {
+int id=view.getId();
+            if(id==R.id.button) { /** Start a new Activity MyCards.java */
+                Button b = (Button) view;
+                buttonText = b.getText().toString();
+                Toast.makeText(getApplicationContext(), buttonText, Toast.LENGTH_SHORT).show();
+            }else if (id==R.id.button2){
+                Button b = (Button) view;
+                buttonText = b.getText().toString();
+                Toast.makeText(getApplicationContext(), buttonText, Toast.LENGTH_SHORT).show();
+            }else if (id==R.id.button3){
+                Button b = (Button) view;
+                buttonText = b.getText().toString();
+                Toast.makeText(getApplicationContext(), buttonText, Toast.LENGTH_SHORT).show();
+            }else if (id==R.id.button4){
+                Button b = (Button) view;
+                buttonText = b.getText().toString();
+                Toast.makeText(getApplicationContext(), buttonText, Toast.LENGTH_SHORT).show();
+            }else if (id==R.id.button5){
+                Button b = (Button) view;
+                buttonText = b.getText().toString();
+                Toast.makeText(getApplicationContext(), buttonText, Toast.LENGTH_SHORT).show();
+            }else if (id==R.id.button6){
+                Button b = (Button) view;
+                buttonText = b.getText().toString();
+                Toast.makeText(getApplicationContext(), buttonText, Toast.LENGTH_SHORT).show();
+            }
+        }
 
 }
